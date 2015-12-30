bfadd35e3ef17f9ca8842cdb1b460dc3850992ea
Hayden Gascoigne <haydengascoigne@gmail.com>
2015-12-30 09:33:47 -0600

    Added functions for getting course info, updated tests.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,32 @@ import Zip from 'adm-zip';
  * @return {Type}
  */
 export default function (filePath) {
-  let zip = new Zip(filePath);
+  let courseZip = new Zip(filePath);
 
-  return zip;
+  let courseFileScanner = {
+    getZipObject: () => {
+      return courseZip;
+    },
+
+    getCourseFiles: () => {
+      return courseZip.getEntries().filter(courseFileScanner._isCourseFile);
+    },
+
+    _isCourseFile: (file) => {
+      return file.entryName.match(/^(?=.*\bcsfiles\/home_dir\b)(?!.*\b.xml\b).*$/ig);
+    },
+
+    getCourseFileIds: () => {
+      return courseFileScanner.getCourseFiles().map(courseFileScanner._getCourseFileId);
+    },
+
+    _getCourseFileId: (file) => {
+      let fileName = file.entryName;
+      let startIndex = fileName.lastIndexOf('xid-') + 4;
+      let endIndex = fileName.indexOf('_', startIndex);
+      return fileName.substring(startIndex, endIndex);
+    }
+  };
+
+  return courseFileScanner;
 }
--- test/index.js
+++ test/index.js
@@ -1,7 +1,23 @@
 import test from 'tape';
 import courseFileScanner from '../src';
 
-test('courseFileScanner', (t) => {
+test('Initialization', (t) => {
   t.plan(1);
-  t.ok(courseFileScanner('../test-course.zip').getEntries().length, 'Makes sure a proper Zip object is returned.');
+  t.ok(courseFileScanner('test-course.zip').getZipObject().getEntries().length, 'Makes sure a proper Zip object is returned.');
+});
+
+test('Get Course Files', (t) => {
+  t.plan(1);
+  let courseZip = courseFileScanner('test-course.zip');
+  let courseFiles = courseZip.getCourseFiles();
+
+  t.equals((Array.isArray(courseFiles) && courseFiles.length > 0), true, 'Makes sure an array of course files is returned.');
+});
+
+test('Get Course File IDs', (t) => {
+  t.plan(1);
+  let courseZip = courseFileScanner('test-course.zip');
+  let courseFileIds = courseZip.getCourseFileIds();
+
+  t.equals((Array.isArray(courseFileIds) && courseFileIds.length > 0), true, 'Makes sure an array of course file IDs is returned.');
 });
